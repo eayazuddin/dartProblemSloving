4dd4b7d546ebe9d2c147ca45a5953c3a871fa543
eayazuddin <91460445+eayazuddin@users.noreply.github.com>
2023-05-16 06:05:38 +0600

    Create oop.dart

---
+++ oop.dart
@@ -0,0 +1,13 @@
+/*
+Create a class called Car. The Car class should have the following properties:
+make: The make of the car (e.g., "Toyota")
+model: The model of the car (e.g., "Corolla")
+year: The year the car was made
+color: The color of the car
+
+The Car class should also have the following methods:
+start(): This method should start the car.
+stop(): This method should stop the car.
+accelerate(): This method should accelerate the car.
+brake(): This method should brake the car.
+*/
